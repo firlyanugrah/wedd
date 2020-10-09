f6e1b5b3ec935246dc620785104b965a7f6a8f8e
Deny <ramandadeny7@gmail.com>
2020-10-09 15:21:33 +0700

    up timeline

--- src/components/OurStory.vue
+++ src/components/OurStory.vue
@@ -7,25 +7,88 @@
         <div class="timeline__line"></div>
         <div class="timeline__linewrap absolute h30vh"></div>
         <div class="timeline__wrapper relative">
-          <div v-for="(a, key) in 8" :key="key">
+          <div v-for="(item, key) in ourstory" :key="key">
             <div class="bullet">
               <img src="/src/assets/icons/bullet.png">
             </div>
             <div class="timeline__linewrap relative h20"></div>
-            <div class="timeline__content relative bg-white">
-              <div class="date">Jan 1, 2011</div>
-              <div class="title">Our First Meet</div>
-              <div class="caption">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s</div>
-              <!-- <div class="image">
-                <img src="/src/assets/images/06.jpg">
-              </div> -->
+            <div class="timeline__content relative bg-white w-full">
+              <div class="timeline__content--inner bg-white text-center w-full md:w-2/3 mx-auto animate__fadeInUp">
+                <div class="date font-fredericka">{{ item.date }}</div>
+                <div class="title font-georgia font-bold">{{ item.title }}</div>
+                <div class="caption font-georgia">{{ item.caption }}</div>
+                <div class="image mt-4" v-if="item.img">
+                  <img :src="item.img">
+                </div>
+              </div>
             </div>
             <div class="timeline__linewrap relative h30vh"></div>
           </div>
         </div>
       </div>
+      <div class="timeline__end pb-40">wkwk</div>
     </section>
 </template>
+
+<script>
+export default {
+  data(){
+    return {
+      ourstory: [{
+        date: 'Jan 1, 2011',
+        title: 'Our First Meet',
+        caption: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s",
+        img: ''
+      },{
+        date: 'Jan 1, 2011',
+        title: 'Our First Meet',
+        caption: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s",
+        img: '/src/assets/images/02.jpg'
+      },{
+        date: 'Jan 1, 2011',
+        title: 'Our First Meet',
+        caption: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s",
+        img: ''
+      },{
+        date: 'Jan 1, 2011',
+        title: 'Our First Meet',
+        caption: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s",
+        img: ''
+      },{
+        date: 'Jan 1, 2011',
+        title: 'Our First Meet',
+        caption: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s",
+        img: '/src/assets/images/02.jpg'
+      }]
+    }
+  },
+  methods:{
+    onScroll(){
+      Array.from(document.getElementsByClassName("timeline__content")).forEach(
+        function(element, index, array) {
+            const rect = element.getBoundingClientRect();
+            rect.top < window.innerHeight-(window.innerHeight*0.63) ? element.classList.add('show') : element.classList.remove('show');
+        }
+    );
+    },
+    registScroll: function (){
+      this.onScroll();
+    },
+  },
+  mounted(){
+    this.$nextTick(function(){
+      // this.registScroll();
+      this.onScroll();
+    });
+  },
+  created(){
+    window.addEventListener('scroll', this.registScroll);
+  },
+  beforeDestroy(){
+    window.removeEventListener('scroll', this.registScroll);
+  }
+}
+</script>
 <style scoped>
   .timeline__line{
     width: 65px;
@@ -38,6 +101,23 @@
   }
   .timeline__linewrap{
     width: 75px;
+    left: 50%;
+    top: 0;
+    transform: translateX(-50%);
+    background-image: url("../../src/assets/icons/line-2.png");
+    background-size: 100%;
+    background-position: 0 0;
+    background-repeat: repeat-y;
+    background-color: rgb(206 153 153 / 20%);
+  }
+  .timeline__content{
+    z-index: 2;
+  }
+  .timeline__content::before{
+    content: '';
+    position: absolute;
+    width: 75px;
+    height: 100%;
     left: 50%;
     top: 0;
     transform: translateX(-50%);
@@ -70,5 +150,17 @@
     width: 100px;
     margin: 0 auto;
     background: rgb(206 153 153 / 20%);
+  }
+  .title{
+    color: #ce9999;
+  }
+  .animate__fadeInUp{
+    opacity: 0;
+    transform: translateY(20%);
+    transition: all .8s cubic-bezier(.46,.54,.17,1);
+  }
+  .timeline__content.show .animate__fadeInUp{
+    opacity: 1;
+    transform: translateY(0%);
   }
 </style>
